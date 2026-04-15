ef9a8be70a1a63379e9528473b345d810b71f2a5
maksymmushta-beep <maksymmushta@gmail.com>
2026-04-15 03:31:52 +0100

    Search and sort functions added

--- routes.js
+++ routes.js
@@ -21,5 +21,7 @@ router.get('/playlist/:id/deletesong/:songid', playlist.deleteSong);
 router.get('/dashboard/deleteplaylist/:id', dashboard.deletePlaylist);
 router.post('/playlist/:id/updatesong/:songid', playlist.updateSong);
 router.get('/stats', stats.createView);
+router.get('/searchCategory', dashboard.createView);
+router.get('/sortData', dashboard.createView);
 
 export default router;
