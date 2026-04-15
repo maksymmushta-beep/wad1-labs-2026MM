3a5e8e9fa711b80594f4e9c266ecc7fb7bef9320
maksymmushta-beep <maksymmushta@gmail.com>
2026-04-15 01:24:00 +0100

    Completed Add Functions Lab

--- routes.js
+++ routes.js
@@ -14,5 +14,7 @@ router.get('/about', about.createView);
 router.get('/playlist/:id', playlist.createView);
 
 router.get('/error', (request, response) => response.status(404).end('Page not found.'));
+router.post('/playlist/:id/addsong', playlist.addSong);
+router.post('/dashboard/addplaylist', dashboard.addPlaylist);
 
 export default router;
